019d7baedabb8642b9cc56278228575427e3777b
navi0105 <ivan000105@gmail.com>
2023-04-21 15:15:12 +0800

    add argument of read_data_from_csv()

--- data_processer/record.py
+++ data_processer/record.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
 
 
@@ -12,15 +12,18 @@ class Record:
     language: str = 'zh'
 
 
-def read_data_from_csv(data_path: str) -> List[Record]:
+def read_data_from_csv(data_path: str,
+                       audio_path_col: str='song_path',
+                       text_with_timestamp_path_col: Optional[str]='lyric_with_char_timestamp_path',
+                       text_col: str='lyric') -> List[Record]:
     assert os.path.exists(data_path)
     records = []
     df = pd.read_csv(data_path)
     
     for idx, row in df.iterrows():
-        record = Record(audio_path=row['song_path'],
-                        text_with_timestamp_path=row['lyric_with_char_timestamp_path'],
-                        text=row['lyric'])
+        record = Record(audio_path=row[audio_path_col],
+                        text_with_timestamp_path=row[text_with_timestamp_path_col] if text_with_timestamp_path_col is not None else '',
+                        text=row[text_col])
         records.append(record)
 
     return records
